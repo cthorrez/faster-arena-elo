2fabed38ccae84d1ec03769cee3908e03fcf498e
cthorrez <claytonthorrez@gmail.com>
2024-09-08 19:27:29 -0700

    now gotta do the base 10 thing :/

--- faster.py
+++ faster.py
@@ -15,52 +15,46 @@ def preprocess_to_numpy(df):
     schedule = np.empty((N,3), dtype=np.int32)
     schedule[:,[0,1]] = df[['model_a', 'model_b']].map(lambda x: model_to_idx[x]).values
     # map outcomes to integers
-    # model_a win -> 0
+    # model_a win -> 2
     # tie         -> 1
-    # model_b win -> 2
+    # model_b win -> 0
     schedule[:,2] = np.select(
         [
             df['winner'] == 'model_a',
             df['winner'] == 'model_b',
         ],
-        [0, 2],
+        [2, 0],
         default=1
     )
     matchups, weights = np.unique(schedule, return_counts=True, axis=0)
     weights = weights.astype(np.float32)
-
-    print(matchups, weights)
-    print(matchups.shape)
-
-    return matchups, weights, models, model_to_idx
+    return matchups, weights, models
 
 
 def bt_loss_and_grad(ratings, matchups, weights, alpha=1.0):
     N = weights.sum()
     matchup_ratings = ratings[matchups[:,[0,1]]]
-    # this maps 0 -> 1.0, 1 -> 0.5, 2 -> 1.0 
+    # this maps 2.0 -> 1.0, 1 -> 0.5, 0 -> 0.0 
     outcomes = matchups[:,2].astype(np.float32) / 2.0
     probs = sigmoid(alpha * (matchup_ratings[:,0] - matchup_ratings[:,1]))
     loss = -(((np.log(probs) * outcomes) + (np.log(1.0 - probs) * (1.0 - outcomes))) * weights).sum() / N
-    matchups_grads = (outcomes - probs) * weights / N
+    matchups_grads = -(outcomes - probs) * weights / N
     model_grad = np.zeros_like(ratings)
-    np.add.at(model_grad, matchups[:, 0], -matchups_grads)
-    np.add.at(model_grad, matchups[:, 1], matchups_grads)
+    np.add.at(model_grad, matchups[:, 0], matchups_grads)
+    np.add.at(model_grad, matchups[:, 1], -matchups_grads)
     return loss, model_grad
 
  
     
-def compute_mle_elo(
-    df, SCALE=400, BASE=10, INIT_RATING=1000, sample_weight=None
-):
+def compute_mle_elo(df, SCALE=400, BASE=10, INIT_RATING=1000):
     start_time = time.time()
-    matchups, weights, models, model_to_idx = preprocess_to_numpy(df)
+    matchups, weights, models = preprocess_to_numpy(df)
     duration = time.time() - start_time
     print(f'preprocess duration (s): {duration}')
     alpha = math.log(BASE) / SCALE
 
     start_time = time.time()
-    initial_ratings = np.zeros(len(model_to_idx))
+    initial_ratings = np.zeros(len(models))
     ratings = minimize(
         fun=bt_loss_and_grad,
         x0=initial_ratings,
@@ -69,10 +63,15 @@ def compute_mle_elo(
     )
     duration = time.time() - start_time
     print(f'fit duration (s): {duration}')
-    # print(ratings)
-    # sort_idx = np.argsort(ratings['x'])
-    # for rank_idx, sort_idx in enumerate(sort_idx):
-    #     print(models[sort_idx], ratings['x'][sort_idx])
+    print(ratings)
+    scaled_ratings = (ratings['x'] * SCALE) + INIT_RATING
+    if "mixtral-8x7b-instruct-v0.1" in models:
+        scaled_ratings += 1114 - scaled_ratings[models.tolist().index("mixtral-8x7b-instruct-v0.1")]
+
+
+    sort_idx = np.argsort(-scaled_ratings)[:10]
+    for rank_idx, sort_idx in enumerate(sort_idx):
+        print(models[sort_idx], scaled_ratings[sort_idx])
 
 
 def get_bootstrap_result(battles, func_compute_elo, num_round):
