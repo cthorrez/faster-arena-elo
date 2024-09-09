090f3b98beab77d79a4a19513205a303844a09e8
cthorrez <claytonthorrez@gmail.com>
2024-09-08 18:36:56 -0700

    bt model works but it's backwards lol

--- faster.py
+++ faster.py
@@ -1,5 +1,6 @@
 import math
 import tqdm
+import time
 import numpy as np
 from scipy.special import expit as sigmoid
 from scipy.optimize import minimize
@@ -31,20 +32,20 @@ def preprocess_to_numpy(df):
     print(matchups, weights)
     print(matchups.shape)
 
-    return matchups, weights, model_to_idx
+    return matchups, weights, models, model_to_idx
 
 
 def bt_loss_and_grad(ratings, matchups, weights, alpha=1.0):
+    N = weights.sum()
     matchup_ratings = ratings[matchups[:,[0,1]]]
     # this maps 0 -> 1.0, 1 -> 0.5, 2 -> 1.0 
     outcomes = matchups[:,2].astype(np.float32) / 2.0
     probs = sigmoid(alpha * (matchup_ratings[:,0] - matchup_ratings[:,1]))
-    print(f'{probs=}')
-    loss = (((np.log(probs) * outcomes) + (np.log(1.0 - probs) * (1.0 - outcomes))) * weights).sum()
-    matchups_grads = (outcomes - probs) * weights
+    loss = -(((np.log(probs) * outcomes) + (np.log(1.0 - probs) * (1.0 - outcomes))) * weights).sum() / N
+    matchups_grads = (outcomes - probs) * weights / N
     model_grad = np.zeros_like(ratings)
-    np.add.at(model_grad, matchups[:, 1], matchups_grads)
     np.add.at(model_grad, matchups[:, 0], -matchups_grads)
+    np.add.at(model_grad, matchups[:, 1], matchups_grads)
     return loss, model_grad
 
  
@@ -52,9 +53,13 @@ def bt_loss_and_grad(ratings, matchups, weights, alpha=1.0):
 def compute_mle_elo(
     df, SCALE=400, BASE=10, INIT_RATING=1000, sample_weight=None
 ):
-    matchups, weights, model_to_idx = preprocess_to_numpy(df)
+    start_time = time.time()
+    matchups, weights, models, model_to_idx = preprocess_to_numpy(df)
+    duration = time.time() - start_time
+    print(f'preprocess duration (s): {duration}')
     alpha = math.log(BASE) / SCALE
 
+    start_time = time.time()
     initial_ratings = np.zeros(len(model_to_idx))
     ratings = minimize(
         fun=bt_loss_and_grad,
@@ -62,7 +67,12 @@ def compute_mle_elo(
         args=(matchups, weights),
         jac=True,
     )
-    print(ratings)
+    duration = time.time() - start_time
+    print(f'fit duration (s): {duration}')
+    # print(ratings)
+    # sort_idx = np.argsort(ratings['x'])
+    # for rank_idx, sort_idx in enumerate(sort_idx):
+    #     print(models[sort_idx], ratings['x'][sort_idx])
 
 
 def get_bootstrap_result(battles, func_compute_elo, num_round):
@@ -73,10 +83,14 @@ def get_bootstrap_result(battles, func_compute_elo, num_round):
     return df[df.median().sort_values(ascending=False).index]
 
 def main():
-    df = pl.scan_parquet('data.parquet').tail(2000).collect().to_pandas()
+    N = 20_000_000
+    df = pl.scan_parquet('data.parquet').tail(N).collect().to_pandas()
     print(df[['model_a', 'model_b', 'winner']])
  
+    start_time = time.time()
     compute_mle_elo(df)
+    duration = time.time() - start_time
+    print(f'duration (s): {duration}')
 
 if __name__ == '__main__':
     main()
