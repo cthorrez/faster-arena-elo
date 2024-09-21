08d6d1aebbd63949dc92dae26b57a0d3b2bf9d9b
Clayton Thorrez <claytonthorrez@gmail.com>
2024-09-20 18:04:22 -0700

    begin the reunification

--- elo_benchmark.py
+++ elo_benchmark.py
@@ -8,11 +8,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from functools import partial
 from data_utils import load_data
-from original import get_bootstrap_result as og_bootstrap, compute_mle_elo as og_mle, compute_elo
+from original import get_bootstrap_result, compute_mle_elo as og_mle, compute_elo as original_compute_elo
 from faster import get_bootstrap_result as fast_bootstrap, compute_mle_elo as fast_mle
 from original_style import construct_style_matrices, fit_mle_elo, get_bootstrap_result_style_control
 from faster_style import construct_style_matrices as construct_style_matrices_fast, compute_style_mle, get_bootstrap_style_mle
-from rating_systems import get_elo_ratings, get_bootstrap_elo_ratings
+from rating_systems import (
+    compute_elo_ratings,
+    compute_bootstrap_elo_ratings
+)
 
 
 @contextmanager
@@ -22,30 +25,36 @@ def timer(task_name=""):
     end_time = time.time()
     print(f"{task_name} duration (s): {end_time - start_time:.4f}")
 
-FUNCTIONS = {
-    'elo': (None, None),
-    'boot_elo': (None, None),
-    'bt': (og_mle, fast_mle),
-    'boot_bt': (og_bootstrap, fast_bootstrap),
-    'style_preprocess': (construct_style_matrices, construct_style_matrices_fast),
-    'style': (fit_mle_elo, compute_style_mle),
-    'boot_style': (get_bootstrap_result_style_control, get_bootstrap_style_mle),
-}
+
+def plot_bootstraps(label, models, original_means, original_stds, new_means, new_stds):
+    x = np.arange(len(original_means))  # Model indices
+    plt.figure(figsize=(10, 6))
+    plt.errorbar(x, original_means, yerr=original_stds, fmt='o-', label=f'Original {label}', capsize=5)
+    plt.errorbar(x, new_means, yerr=new_stds, fmt='s-', label=f'New {label}', capsize=5)
+    plt.xticks(ticks=x, labels=models, rotation=45, ha='right')
+    plt.xlabel('Model')
+    plt.ylabel('Rating')
+    plt.title(f'Bootstrap {label}')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f'figs/bootstrap_{label}')
+    plt.show()
+
 
 def bench_elo(df):
     with timer('original elo'):
-        original_ratings = compute_elo(df)
+        original_ratings = original_compute_elo(df)
     with timer('new elo'):
-        new_ratings = get_elo_ratings(df)
+        new_ratings = compute_elo_ratings(df)
     diffs = [original_ratings[m] - new_ratings[m] for m in original_ratings.keys()]
     print(f"mean abs diff: {np.mean(np.abs(diffs))}")
 
 
 def bench_bootstrap_elo(df, num_round):
     with timer('original elo'):
-        original_ratings = og_bootstrap(df, compute_elo, num_round, seed=42)
+        original_ratings = get_bootstrap_result(df, original_compute_elo, num_round, seed=42)
     with timer('new elo'):
-        new_ratings = get_bootstrap_elo_ratings(df, num_round=num_round)
+        new_ratings = compute_bootstrap_elo_ratings(df, num_round=num_round)
     
     original_means = original_ratings.values.mean(axis=0)
     new_means = new_ratings[original_ratings.columns].values.mean(axis=0)
@@ -55,49 +64,19 @@ def bench_bootstrap_elo(df, num_round):
     new_stds = new_ratings[original_ratings.columns].values.std(axis=0)
     print(f'mean abs diff in bootstrap stds: {np.mean(np.abs(original_stds - new_stds))}')
 
-    model_names = original_ratings.columns
-    x = np.arange(len(original_means))  # Model indices
-    plt.figure(figsize=(10, 6))
-    plt.errorbar(x, original_means, yerr=original_stds, fmt='o-', label='Original Elo', capsize=5)
-    plt.errorbar(x, new_means, yerr=new_stds, fmt='s-', label='New Elo', capsize=5)
-    plt.xticks(ticks=x, labels=model_names, rotation=45, ha='right')
-    plt.xlabel('Model')
-    plt.ylabel('Elo Rating')
-    plt.title('Bootstrap Elo Ratings with Error Bars')
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-def bench_bootstrap_elo_seed(df, num_round):
-    ratings_42 = og_bootstrap(df, compute_elo, num_round=num_round, seed=42)
-    ratings_0 = og_bootstrap(df, compute_elo, num_round=num_round, seed=0)
-    
-    original_means = ratings_42.values.mean(axis=0)
-    new_means = ratings_0[ratings_42.columns].values.mean(axis=0)
-    print(f'mean abs diff in bootstrap means: {np.mean(np.abs(original_means - new_means))}')
-
-    original_stds = ratings_42.values.std(axis=0)
-    new_stds = ratings_0[ratings_42.columns].values.std(axis=0)
-    print(f'mean abs diff in bootstrap stds: {np.mean(np.abs(original_stds - new_stds))}')
-
-    model_names = ratings_42.columns
-    x = np.arange(len(original_means))  # Model indices
-    plt.figure(figsize=(10, 6))
-    plt.errorbar(x, original_means, yerr=original_stds, fmt='o-', label='Boot Elo Seed 42', capsize=5)
-    plt.errorbar(x, new_means, yerr=new_stds, fmt='s-', label='Boot Elo Seed 0', capsize=5)
-    plt.xticks(ticks=x, labels=model_names, rotation=45, ha='right')
-    plt.xlabel('Model')
-    plt.ylabel('Elo Rating')
-    plt.title('Bootstrap Elo Ratings with Error Bars')
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+    plot_bootstraps(
+        label='Elo',
+        models=original_ratings.columns,
+        original_means=original_means,
+        original_stds=original_stds,
+        new_means=new_means,
+        new_stds=new_stds
+    )
 
 
 
 def main():
-    N = 2_000_000
+    N = 200_000
     df = load_data(N=N, use_preprocessed=True)
     # df = load_data()
 
--- rating_systems.py
+++ rating_systems.py
@@ -20,32 +20,58 @@ STYLE_CONTROL_ELEMENTS_V1 = [
 ]
 
 
+# def preprocess_to_numpy(df):
+#     models = np.unique(df[['model_a', 'model_b']].values)
+#     model_to_idx = {model:idx for idx,model in enumerate(models)}
+#     matchups = df[['model_a', 'model_b']].map(lambda x: model_to_idx[x]).values
+#     outcomes = np.full(shape=(matchups.shape[0],), fill_value=0.5)
+#     outcomes[df["winner"] == "model_a"] = 1.0
+#     outcomes[df["winner"] == "model_b"] = 0.0
+#     return matchups, outcomes, models
+
+# def preprocess_to_numpy(df):
+#     # Use pd.factorize() for both model columns at once
+#     models, matchups = pd.factorize(df[['model_a', 'model_b']].values.ravel())
+#     matchups = matchups.reshape(-1, 2)
+#     print(matchups)
+#     # Pre-allocate outcomes array with correct values
+#     outcomes = np.where(df["winner"] == "model_a", 1.0, 
+#                         np.where(df["winner"] == "model_b", 0.0, 0.5))
+    
+#     return matchups, outcomes, pd.unique(models)
+
 def preprocess_to_numpy(df):
-    models = np.unique(df[['model_a', 'model_b']].values)
-    model_to_idx = {model:idx for idx,model in enumerate(models)}
-    matchups = df[['model_a', 'model_b']].map(lambda x: model_to_idx[x]).values
-    outcomes = np.full(shape=(matchups.shape[0],), fill_value=0.5)
-    outcomes[df["winner"] == "model_a"] = 1.0
-    outcomes[df["winner"] == "model_b"] = 0.0
-    return matchups, outcomes, models
+    # Combine model_a and model_b, then factorize
+    all_models = pd.concat([df['model_a'], df['model_b']])
+    models, model_indices = pd.factorize(all_models)
+    
+    # Split the factorized indices back into model_a and model_b
+    n_rows = len(df)
+    model_a_indices = models[:n_rows]
+    model_b_indices = models[n_rows:]
+    
+    # Create matchups array
+    matchups = np.column_stack([model_a_indices, model_b_indices])
+    
+    # Create outcomes array
+    outcomes = np.where(df["winner"] == "model_a", 1.0, 
+                        np.where(df["winner"] == "model_b", 0.0, 0.5))
+    
+    return matchups, outcomes, model_indices.to_numpy()
 
 
 def fit_vectorized_elo(
     matchups,
     outcomes,
+    sample_indices,
     num_models,
     k=4.0,
     base=10.0,
     init_rating=1000.0,
     scale=400.0,
-    sample_indices=None,
     ):
     """fit multiple sets of Elo ratings on different samples of the data at the same time"""
     alpha = math.log(base) / scale
-    n = matchups.shape[0]
-    # if no sample indices are provided, just go over the results in order once
-    if sample_indices is None:
-        sample_indices = np.arange(n)[:,np.newaxis]
     num_samples = sample_indices.shape[1]
     ratings = np.zeros(shape=(num_samples, num_models), dtype=np.float64)
     # iterate over the rows of sample_indices, each column is an index into a match in the input arrays
@@ -60,17 +86,23 @@ def fit_vectorized_elo(
         updates = k * (sample_outcomes - probs)
         ratings[sample_range, model_a_indices] += updates
         ratings[sample_range, model_b_indices] -= updates
-    return np.squeeze(ratings) + init_rating
+    return ratings + init_rating
 
-def get_elo_ratings(df, k=4.0, base=10.0, init_rating=1000.0, scale=400.0):
+def compute_elo_ratings(df, k=4.0, base=10.0, init_rating=1000.0, scale=400.0):
     matchups, outcomes, models = preprocess_to_numpy(df)
-    ratings = fit_vectorized_elo(matchups, outcomes, len(models),k, base, init_rating, scale, sample_indices=None)
+    alpha = math.log(base) / scale
+    ratings = np.full(shape=(len(models),), fill_value=init_rating)
+    for (model_a_idx, model_b_idx), outcome in zip(matchups, outcomes):
+        prob = 1.0 / (1.0 + math.exp(alpha * (ratings[model_b_idx] - ratings[model_a_idx])))
+        update = k * (outcome - prob)
+        ratings[model_a_idx] += update
+        ratings[model_b_idx] -= update
     return {model:ratings[idx] for idx,model in enumerate(models)}
 
-def get_bootstrap_elo_ratings(df, num_round=100, k=4.0, base=10.0, init_rating=1000.0, scale=400.0):
+def compute_bootstrap_elo_ratings(df, num_round=100, k=4.0, base=10.0, init_rating=1000.0, scale=400.0):
     matchups, outcomes, models = preprocess_to_numpy(df)
     sample_indices = np.random.randint(low=0, high=len(df), size=(len(df), num_round))
-    ratings = fit_vectorized_elo(matchups, outcomes, len(models), k, base, init_rating, scale, sample_indices)
+    ratings = fit_vectorized_elo(matchups, outcomes, sample_indices, len(models), k, base, init_rating, scale)
     df = pd.DataFrame(data=ratings, columns=models)
     return df[df.median().sort_values(ascending=False).index]
 
